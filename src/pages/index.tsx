29e9738261b9ac132b9f7e1a85dd0e08077d09ad
David May <David.May@fiery.com>
2025-08-08 15:09:15 -0700

    [pages] rename journal.page to hikes.page for consistency

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 // All Pages
 import { GoalsPage } from "./goals.page";
-import { HikesPage } from "./journal.page";
+import { HikesPage } from "./hikes.page";
 import { ParksPage } from "./parks.page";
 import { PlansPage } from "./plans.page";
 
