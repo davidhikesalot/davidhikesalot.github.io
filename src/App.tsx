4a4067950e289eeb4bf21847efbb83675b64ca12
David May <David.May@fiery.com>
2025-08-08 21:45:06 -0700

    add loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import "./App.scss";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { ISiteData, fetchSiteData } from "./services/data.service";
 import { IPageLayoutProps, PageLayout } from "./layouts/page.layout";
 import { AppHeader } from "./components/appheader.component";
+import Stack from "react-bootstrap/esm/Stack";
 
 export default function App() {
   const [data, setData] = useState({});
-  const [isLoaded, setIsLoaded] = useState({});
+  const [isLoaded, setIsLoaded] = useState(false);
   const props: IPageLayoutProps = { data };
 
   useEffect(() => {
+    console.log("Fetching site data...");
     fetchSiteData().then((siteData: ISiteData) => {
       setData((prev) => ({ ...siteData }));
-      setIsLoaded((prev) => true);
+      setIsLoaded(true);
+      console.log("Site data fetched successfully");
     });
   }, []);
 
@@ -26,7 +30,17 @@ export default function App() {
           <Outlet />
         </PageLayout>
       )}
+      {!isLoaded && (
+        <Stack className="mt-5">
+          <ClimbingBoxLoader 
+            size={20}
+            color="var(--app-heading-background-color)"
+            cssOverride={{ margin: "auto" }}
+          />
+        </Stack>
+      )}
       <footer></footer>
     </div>
   );
 }
+  
